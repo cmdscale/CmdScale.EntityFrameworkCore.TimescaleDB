bc571ce19e2c594c6b47779a2966fc86dc56a6b9
Sebastian Ederer <sebastian.ederer@cmdscale.com>
2025-08-25 16:58:23 +0200

    feat: fixed another bug with project refernces and migrations

--- CmdScale.EntityFrameworkCore.TimescaleDB.Example/TimescaleDBDesignTimeService.cs
+++ CmdScale.EntityFrameworkCore.TimescaleDB.Example/TimescaleDBDesignTimeService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
+﻿using CmdScale.EntityFrameworkCore.TimescaleDB.Design;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Migrations.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CmdScale.EntityFrameworkCore.TimescaleDB.Example
@@ -8,6 +11,12 @@ namespace CmdScale.EntityFrameworkCore.TimescaleDB.Example
         public void ConfigureDesignTimeServices(IServiceCollection services)
         {
             Console.WriteLine("=== This is another design time in the consumer project to test if it will overwrite the ICSharpMigrationOperationGenerator back to default. ===");
+
+            // NOTE: When using project references, instead of package referneces, you need to uncomment these lines to find inject the correct ICSharpMigrationOperationGenerator
+            // The reason for this is, because the CmdScale.EntityFrameworkCore.TimescaleDB.Design project only copies the required assembly-attribute when being packaged.
+
+            //services.AddSingleton<ICSharpMigrationOperationGenerator, TimescaleCSharpMigrationOperationGenerator>()
+            //        .AddSingleton<IDatabaseModelFactory, TimescaleDatabaseModelFactory>();
         }
     }
 }
